ead820288635a3543e528815c2e387ae76a8c16e
DavidLake23 <51886804+DavidLake23@users.noreply.github.com>
2020-03-13 16:38:15 -0500

    Desarrollado para problema 1

---
+++ FundamentosEAFIT/src/Taller6/PlanetaCopia.java
@@ -0,0 +1,52 @@
+package Taller6;
+
+public class PlanetaCopia {
+	
+	private String nombre;
+	private double masa;
+	public static int contador = 0;
+	
+	@SuppressWarnings("unused")
+	public PlanetaCopia() {
+		
+		PlanetaCopia vPlaneta = new PlanetaCopia("Tierra",5972);
+	}
+	
+	public PlanetaCopia(String n, double m) {
+		
+		this.nombre = n;
+		this.masa = m;
+		contador++;
+		
+		if(m >= 5.8) {
+			
+			PrincipalPlaneta.planetasMalignos++;
+		}
+	}
+	
+	public String getNombre() {
+		
+		return this.nombre;
+	}
+	
+	public double getMasa() {
+		
+		return this.masa;
+	}
+	
+	public void setNombre(String n) {
+		
+		this.nombre = n;
+	}
+	
+	public void setMasa(double m) {
+		
+		this.masa = m;
+	}
+	
+	public static PlanetaCopia planetaMedio(PlanetaCopia p) {
+		
+		PlanetaCopia vPlaneta = new PlanetaCopia(p.getNombre(), p.getMasa() / 2);
+		return vPlaneta;
+	}
+}
